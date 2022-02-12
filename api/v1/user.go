1121f4c14c001566dcc3384cc428a0ea2a5ed2b4
unknown <lxlandsx@outlook.com>
2022-02-12 11:17:33 +0800

    完成user接口的编写，并且使用Scrypt对密码进行加

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,30 +1,85 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"ginblog/model"
+	"ginblog/utils/errmsg"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
-// UserExist 查询用户是否存在
-func UserExist(c *gin.Context) {
-
-}
+var code int
 
 // AddUser 添加用户
 func AddUser(c *gin.Context) {
 	// todo 添加用户
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+	code = model.CheckUser(data.Username)
+	if code == errmsg.SUCCESS {
+		model.CreateUser(&data)
+	}
+	if code == errmsg.ERROR_USERNAME_USED {
+		code = errmsg.ERROR_USERNAME_USED
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 // 查询单个用户
 
 // GetUsers 查询用户列表
 func GetUsers(c *gin.Context) {
+	pageSize, _ := strconv.Atoi(c.Query("pageisize"))
+	pageNum, _ := strconv.Atoi(c.Query("pagenum"))
 
+	if pageSize == 0 {
+		pageSize = -1
+	}
+	if pageNum == 0 {
+		pageNum = 2
+	}
+
+	data := model.GetUsers(pageSize, pageNum)
+	code = errmsg.SUCCESS
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 // EditUser 编辑用户
 func EditUser(c *gin.Context) {
-
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+	//if err != nil {
+	//	fmt.Println("错误为", err)
+	//}
+	//fmt.Println("绑定的数据为", data.Username)
+	code = model.CheckUser(data.Username)
+	id, _ := strconv.Atoi(c.Param("id"))
+	if code == errmsg.SUCCESS {
+		model.EditUser(id, &data)
+	}
+	if code == errmsg.ERROR_USERNAME_USED {
+		c.Abort()
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 // DeleteUser 删除用户
 func DeleteUser(c *gin.Context) {
-
+	id, _ := strconv.Atoi(c.Param("id"))
+	code = model.DeleteUser(id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
--- model/User.go
+++ model/User.go
@@ -1,6 +1,12 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"encoding/base64"
+	"ginblog/utils/errmsg"
+	"golang.org/x/crypto/scrypt"
+	"gorm.io/gorm"
+	"log"
+)
 
 type User struct {
 	gorm.Model
@@ -9,4 +15,71 @@ type User struct {
 	Role     int    `gorm:"type:int" json:"role"` // 0是管理员 1是阅读者
 }
 
-// 新增用户
+// CheckUser 查询用户是否存在
+func CheckUser(username string) int {
+	var users User
+	//fmt.Println("用户名为", username)
+	db.Debug().Select("id").Where("username = ?", username).First(&users)
+	if users.ID > 0 {
+		return errmsg.ERROR_USERNAME_USED
+	}
+	return errmsg.SUCCESS
+}
+
+// CreateUser 新增用户
+func CreateUser(data *User) int {
+	data.Password = ScryptPassword(data.Password)
+	err = db.Create(&data).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+// GetUsers 查询用户列表
+func GetUsers(pageSize int, pageNum int) []User {
+	var users []User
+	err = db.Debug().Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&users).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil
+	}
+	return users
+}
+
+// EditUser 编辑用户信息(密码以外的内容)
+func EditUser(id int, data *User) int {
+	var maps = make(map[string]interface{})
+	maps["username"] = data.Username
+	maps["role"] = data.Role
+	err = db.Debug().Model(&User{}).Where("id = ?", id).Updates(maps).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+// DeleteUser 删除用户
+func DeleteUser(id int) int {
+	err = db.Debug().Where("id = ?", id).Delete(&User{}).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+// ScryptPassword 密码加密
+func ScryptPassword(password string) string {
+	const KeyLen = 10
+	salt := make([]byte, 8)
+	salt = []byte{12, 32, 4, 5, 6, 66, 22, 111}
+	HashPassword, err := scrypt.Key([]byte(password), salt, 16384, 8, 1, KeyLen)
+	if err != nil {
+		log.Fatal(err)
+	}
+	FinalPassword := base64.StdEncoding.EncodeToString(HashPassword)
+	return FinalPassword
+}
+
+func (u *User) BeforeCreate() {
+
+}
